02e1d10f0e539f5630f8c929d1d9b82a8377fd7a
Jairo Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2022-10-21 22:58:24 -0500

    Delete Wordle directory

--- Wordle/Wordle.py
+++
@@ -1,6 +0,0 @@
-import Wordle_Logica_Juego
-print("¡Bienvenido a Wordle!\n")
-Wordle_Logica_Juego.Wordle_Logica.Bienvenida()
-print("A continuacion deberas ingresar una palabra para empezar a jugar. \n¡Recuerda que solo tienes 6 intentos para adivinar la palabra!, suerte ;)\n")
-Wordle_Logica_Juego.Wordle_Logica.Validar_palabra_intento()
-Wordle_Logica_Juego.Wordle_Logica.Palabras_Iguales()
